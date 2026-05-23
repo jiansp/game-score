551a1a8c3cfb958e469d0deec7e5a2108beae500
jiansp <kkjane@163.com>
2026-05-23 22:42:01 +0800

    初版，卡在了修改图标这里

--- app/src/main/java/com/spheign/gamescore/MainActivity.kt
+++ app/src/main/java/com/spheign/gamescore/MainActivity.kt
@@ -60,6 +60,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_GameScore)
         setContentView(R.layout.activity_main)
 
         prefs = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
